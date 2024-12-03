39b59088613a16fb3841f77b722f9ed0dbe5f81a
Duongg <lengocduong003@gmail.com>
2024-12-04 01:27:40 +0700

    fix

--- src/components/login/login_server_actions.ts
+++ src/components/login/login_server_actions.ts
@@ -1,7 +1,6 @@
 'use server'
 import API from "@/api/api";
 import { FetchServerPostApiNoToken } from "@/api/fetch_server_api";
-import { redirect } from 'next/navigation'; // Import hàm redirect
 import { setSessionId } from "@/utils/session_store"
 export const LoginServerActions = async (AuthenticationRequest: AuthenticationRequest) => {
 
@@ -10,7 +9,7 @@ export const LoginServerActions = async (AuthenticationRequest: AuthenticationRe
     // thanh cong
     if (res.status === 200) {
         setSessionId(res.result.token);
-        redirect("/")
+
     }
     return res;
 }
